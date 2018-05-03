2b7bc2b8886ec362015f2a2ca0487d7a87140756
Michael Kitzan <mkitzan45@gmail.com>
2018-05-02 21:20:00 -0700

    efuncts comments
    Added comments to efuncts.h
    Thinking of changing this file name to a holistic helper function interface...

--- efuncts.h
+++ efuncts.h
@@ -1,9 +1,13 @@
 #ifndef _EFUNCTS_H_
 #define _EFUNCTS_H_
 
+/* wrapper for malloc
+   checks if malloc returns NULL pointer, and exits if so */
 void *emalloc(size_t);
 
-
+/* wrapper for memcpy
+   checks if copying on the same address, and does not memcpy if so
+   cleans up valgrind output */
 void ememcpy(void *, void *, size_t);
 
 #endif
